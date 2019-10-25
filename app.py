9414fb0c60975f1b495a8ba6e27958d03015a963
Ubuntu <ubuntu@ip-172-31-86-230.ec2.internal>
2019-10-25 04:59:18 +0000

    successfully uploaded static image url to database, and retrieve image to display in restaurants.template.html .

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
-import pymongo
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+import pymongo 
 import os
 
 app = Flask(__name__)
+
+#configure uploads
+TOP_LEVEL_DIR = os.path.abspath(os.curdir)
+upload_dir = '/static/uploads/img/'
+app.config["UPLOADS_DEFAULT_DEST"] = TOP_LEVEL_DIR + upload_dir
+app.config["UPLOADED_IMAGES_DEST"] = TOP_LEVEL_DIR + upload_dir
+app.config["UPLOADED_IMAGES_URL"] = upload_dir
+
+images_upload_set = UploadSet('images', IMAGES)
+configure_uploads(app, images_upload_set)
+#end configure uploads
 
 # 1. Retrieve the environment variables
 MONGO_URI = os.getenv('MONGO_URI')
@@ -32,13 +44,19 @@ def index():
 def restaurants():
     # Fetch all the existing todos as a Python dictionary
     results = conn[DATABASE_NAME][RESTAURANTS].find({})
+    # Fetch all images stored and return them
+    all_images = conn[DATABASE_NAME]['image_url'].find({}); #1
     #Return a template and assign the results to a placeholder in that template
-    return render_template('restaurants.template.html', data=results)
+    return render_template('restaurants.template.html', data=results, all_images=all_images)
+
 
 #Route to show the 'add/create restaurant' form    
 @app.route('/add_restaurant')
 def add_restaurant():
     return render_template('add-restaurant.template.html')   
+
+
+
     
 #Route to process the 'add/create restaurant' form
 @app.route('/add_restaurant', methods=['POST'])
@@ -48,6 +66,10 @@ def process_add_restaurant():
     business_hours = request.form.get('business_hours')
     telephone = request.form.get('telephone')
     email_address = request.form.get('email_address')
+    image = request.files.get('image') #1 -- get the uploaded image
+    filename = images_upload_set.save(image) #2 -- save uploaded image    
+
+
     
     #Create a new restaurant
     conn[DATABASE_NAME][RESTAURANTS].insert({
@@ -55,7 +77,8 @@ def process_add_restaurant():
         'restaurant_address': restaurant_address,
         'business_hours': business_hours,
         'telephone': telephone,
-        'email_address': email_address
+        'email_address': email_address,
+        'image_url' : images_upload_set.url(filename) #3 save image url
     })
 
     return redirect(url_for('index'))
